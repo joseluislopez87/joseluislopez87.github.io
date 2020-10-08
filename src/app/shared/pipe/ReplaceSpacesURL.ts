04aa09f2adf1816b9887d3d8f8d4ca2f8279259a
joseluislopez87 <joseluislopez87@gmail.com>
2020-10-08 16:35:01 +0000

    Deploying to dist from  @ 2b8d3ad9255ef335b32ca0b280e6393589ee4bd6 🚀

--- src/app/shared/pipe/ReplaceSpacesURL.ts
+++
@@ -1,7 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-@Pipe({name: 'replaceSpacesURL'})
-export class ReplaceSpacesURL implements PipeTransform {
-  transform(value: string): string {
-    return value.replace(' ', '%20');
-  }
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  formUrl: "https://formcarry.com/s/Fd4beVRoj"
-};
